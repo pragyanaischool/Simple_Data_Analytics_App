aebb4f7d72708410d5b550fc77ca853618cd9bb4
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2024-03-31 16:48:42 +0530

    Create Data_anlytics_app.py

---
+++ Data_anlytics_app.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+from sklearn import datasets
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+st.title('My Data Analytics App')  # Sets the title of your page
+
+st.subheader('Data Loading Section')  # Sets a header for a section
+
+# Loading data from URL
+url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/daily-min-temperatures.csv'
+df = pd.read_csv(url, parse_dates=['Date'])
+
+# Display DataFrame in Streamlit
+st.dataframe(df)
